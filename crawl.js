12ac7326dc34cdb42b499ddbb7839a901e570331
Furkan Kovan <furkankovankaya@gmail.com>
2023-04-14 22:34:36 +0300

    added crawlPage with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,14 @@
 const { JSDOM } = require('jsdom')
 
+function standardizeURL(urlString) {
+    const urlObj = new URL(urlString)
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
+    }
+    return hostPath
+}
+
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -26,17 +35,29 @@ function getURLsFromHTML(htmlBody, baseURL){
     return urls
 }
 
-function standardizeURL(urlString) {
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1)
-    }
+async function crawlPage(currentURL) {
+    console.log(`Currently crawling: ${currentURL}`)
 
-    return hostPath
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code: ${resp.status}, on page ${currentURL}`)
+            return
+        }
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes("text/html")) {
+            console.log(`Non html response! Content type: ${contentType}, on page ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+
+    } catch(err) {
+        console.log(`Error in fetch: ${err.message}, on page ${currentURL}`)
+    }
 }
 
 module.exports = {
     standardizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
